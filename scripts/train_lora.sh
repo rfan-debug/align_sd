de63d189b7278af9c9c77072f29b14b17772bde5
Ruixi Fan <ruixi@treat.xyz>
2023-09-19 20:46:48 -0700

    further reduce

--- scripts/train_lora.sh
+++ scripts/train_lora.sh
@@ -3,7 +3,7 @@ accelerate launch train_text_to_image_lora.py \
 --annotation_file annotation.jsonl \
 --regularization_annotation regularization_images.jsonl \
 --resolution 512 \
---train_batch_size 80 \
+--train_batch_size 10 \
 --num_train_epochs 99 \
 --checkpointing_steps 5000 \
 --learning_rate 1e-5 \
