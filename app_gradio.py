f8827835cf92ee2832b3afc79fa18ff3b26f377c
Ruixi Fan <ruixi@treat.xyz>
2023-09-21 10:55:21 -0700

    use the trained model

--- app_gradio.py
+++ app_gradio.py
@@ -42,7 +42,7 @@ def create_model(weight_path):
     return model
 
 original = create_model("")
-adapted = create_model("adapted_model.bin")
+adapted = create_model("lora_output2/pytorch_lora_weights.bin")
 
 def inference(prompt):
     # create a hash of the prompt
