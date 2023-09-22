3d033e184c6578c38706317a7d7b6c12c34bff33
Ruixi Fan <ruixi@treat.xyz>
2023-09-21 17:23:27 -0700

    retry

---
+++ scripts/train_lora_sdxl.sh
@@ -0,0 +1,19 @@
+accelerate launch train_text_to_image_lora.py \
+--pretrained_model_name_or_path "stabilityai/stable-diffusion-xl-base-1.0" \
+--annotation_file annotation.jsonl \
+--regularization_annotation regularization_images.jsonl \
+--resolution 512 \
+--train_batch_size 1 \
+--num_train_epochs 99 \
+--checkpointing_steps 5000 \
+--learning_rate 1e-5 \
+--lr_scheduler constant \
+--lr_warmup_steps 0 \
+--seed 42 \
+--output_dir "./lora_output_sdxl" \
+--validation_prompt_file validation_prompts.txt \
+--validation_iters 5000 \
+--report_to wandb \
+--resume_from_checkpoint latest \
+--dataloader_num_workers 8 \
+--max_train_steps 5000
