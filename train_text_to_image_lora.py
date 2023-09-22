f11a4a477b38880827d331f2672cd19963bb5b6f
Ruixi Fan <ruixi@treat.xyz>
2023-09-21 17:20:19 -0700

    redo the sizes

--- train_text_to_image_lora.py
+++ train_text_to_image_lora.py
@@ -815,15 +815,16 @@ def main(args):
         pixel_values = torch.stack([example["pixel_values"] for example in examples])
         pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
 
-        # Do we need the following ones?
-        # original_sizes = [example["original_sizes"] for example in examples]
-        # crop_top_lefts = [example["crop_top_lefts"] for example in examples]
+        original_sizes = [example["original_size"] for example in examples]
+        crop_top_lefts = [example["crop_top_left"] for example in examples]
         input_ids_one = torch.cat([example["input_ids_one"] for example in examples])
         input_ids_two = torch.cat([example["input_ids_two"] for example in examples])
         return {
             "pixel_values": pixel_values,
             "input_ids_one": input_ids_one,
             "input_ids_two": input_ids_two,
+            "original_sizes": original_sizes,
+            "crop_top_lefts": crop_top_lefts,
         }
 
     # DataLoaders creation:
