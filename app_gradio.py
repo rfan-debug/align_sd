a2ed5f8d186d2e8895b073c5bf7d5b3c12822501
Ruixi Fan <ruixi@treat.xyz>
2023-09-22 09:58:30 -0700

    adapt gradio

--- app_gradio.py
+++ app_gradio.py
@@ -1,6 +1,6 @@
 import torch
-from diffusers.models.cross_attention import LoRACrossAttnProcessor
-from diffusers import StableDiffusionPipeline
+import torch.nn.functional as F
+from diffusers.models.attention_processor import LoRAAttnProcessor2_0, LoRAAttnProcessor
 from diffusers import StableDiffusionXLPipeline
 
 import hashlib
@@ -9,7 +9,7 @@ import gradio as gr
 
 # load a pretrained sd model, and then load the LoRA weights
 def create_model(weight_path):
-    model = StableDiffusionPipeline.from_pretrained(
+    model = StableDiffusionXLPipeline.from_pretrained(
         "stabilityai/stable-diffusion-xl-base-1.0",
         torch_dtype=torch.float16,
         variant="fp16",
@@ -25,6 +25,7 @@ def create_model(weight_path):
     lora_rank = lora_rank[0]
     for name in unet.attn_processors.keys():
         cross_attention_dim = None if name.endswith("attn1.processor") else unet.config.cross_attention_dim
+        hidden_size = None
         if name.startswith("mid_block"):
             hidden_size = unet.config.block_out_channels[-1]
         elif name.startswith("up_blocks"):
@@ -34,9 +35,15 @@ def create_model(weight_path):
             block_id = int(name[len("down_blocks.")])
             hidden_size = unet.config.block_out_channels[block_id]
 
-        lora_attn_procs[name] = LoRACrossAttnProcessor(
-            hidden_size=hidden_size, cross_attention_dim=cross_attention_dim, rank=lora_rank
-        ).to("cuda")
+        if hasattr(F, "scaled_dot_product_attention"):
+            lora_attn_procs[name] = LoRAAttnProcessor2_0(
+                hidden_size=hidden_size, cross_attention_dim=cross_attention_dim, rank=lora_rank,
+            ).to("cuda")
+        else:
+            lora_attn_procs[name] = LoRAAttnProcessor(
+                hidden_size=hidden_size, cross_attention_dim=cross_attention_dim, rank=lora_rank,
+            ).to("cuda")
+
     unet.set_attn_processor(lora_attn_procs)
     unet.load_state_dict(model_weight, strict=False)
     return model
