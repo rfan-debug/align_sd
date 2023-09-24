1f385e9e31c539838091d50b6aec0fe5a168956b
Ruixi Fan <ruixi@treat.xyz>
2023-09-24 05:26:13 +0000

    new editing

--- scripts/.ipynb_checkpoints/train_lora-checkpoint.sh
+++ scripts/.ipynb_checkpoints/train_lora-checkpoint.sh
@@ -3,7 +3,7 @@ accelerate launch train_text_to_image_lora.py \
 --annotation_file annotation.jsonl \
 --regularization_annotation regularization_images.jsonl \
 --resolution 512 \
---train_batch_size 10 \
+--train_batch_size 2 \
 --num_train_epochs 99 \
 --checkpointing_steps 5000 \
 --learning_rate 1e-5 \
@@ -16,4 +16,4 @@ accelerate launch train_text_to_image_lora.py \
 --report_to wandb \
 --resume_from_checkpoint latest \
 --dataloader_num_workers 8 \
---max_train_steps 5000
+--max_train_steps 20000
